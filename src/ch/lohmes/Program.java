fcfc99182531a827c4ea8d101a65c348b8a10e29
LAPTOP-I3FK83BQ\Duarte <dudanos1205@gmail.com>
2019-03-26 16:02:25 +0100

    Added the JavaFX lib Configured main method and launch method

--- src/ch/lohmes/Program.java
+++ src/ch/lohmes/Program.java
@@ -1,9 +1,23 @@
 package ch.lohmes;
 
-public class Program {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Program extends Application {
 
 	public static void main(String[] args) {
-
+		launch(args);
+	}
+  
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Views/MainWindow.fxml"));
+        primaryStage.setTitle("Registration Form FXML Application");
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
 	}
 
 }
